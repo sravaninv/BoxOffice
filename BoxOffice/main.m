056e36e4a54cb01424b78cf8c36ac177cee94c7a
sravaninv <vijayasravanin@gmail.com>
2017-04-07 15:27:09 -0500

    Initial Commit

---
+++ BoxOffice/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BoxOffice
+//
+//  Created by Sravani on 4/7/17.
+//  Copyright © 2017 sravani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
